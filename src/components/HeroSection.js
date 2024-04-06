8b244f6c8f4b7c47a195bba01c9da7c19ad34259
Met0l <pepenarmaria@gmail.com>
2024-04-07 02:47:23 +0300

    ilies sters

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,11 +11,6 @@ function HeroSection() {
       <h2>COLEGIUL NATIONAL "DECEBAL" DEVA</h2>
       <p>Centru educational de excelenta si nucleu cultural emergent</p>
       <div class="hero-image">
-        <div class="hero-text">
-          <h1>Illies Marin Florin</h1>
-          <p>Director Colegiul National "Decebal" Deva</p>
-          <button>Contact</button>
-        </div>
       </div>
     </div>
   )
