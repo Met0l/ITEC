410f3109c1732eb611a9d85ec212bac22d4c1ffc
Met0l <pepenarmaria@gmail.com>
2024-04-07 03:02:47 +0300

    modificat HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,15 +2,16 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
-import {Link} from 'react-router-dom';
 
 function HeroSection() {
   return (
     <div className="hero-container">
       <img src="/colegiul_national_Decebal_h1.jpg"/>
-      <h2>COLEGIUL NATIONAL "DECEBAL" DEVA</h2>
-      <p>Centru educational de excelenta si nucleu cultural emergent</p>
-      <div class="hero-image">
+      <div className='hero-section'>
+        <div className='hero-title'>COLEGIUL NATIONAL "DECEBAL" DEVA</div>
+        <div className='hero-text'>
+          <p>Centru educational de excelenta si nucleu cultural emergent</p>
+        </div>
       </div>
     </div>
   )
